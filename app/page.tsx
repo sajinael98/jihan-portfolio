4db32e1ae64298209d608733a2fed94df21cb9c8
sajinale98 <sajinael00@gmail.com>
2024-05-04 16:58:22 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { Box, } from '@mantine/core';
+import { motion } from 'framer-motion';
+import { useWindowScroll } from '@mantine/hooks';
+
+export default function HomePage() {
+  return (
+    <>
+      jihan
+    </>
+  );
+}
